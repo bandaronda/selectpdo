97a97f79d32fdabeb36f18e85b71d55dab391859
bandaronda <131749924+bandaronda@users.noreply.github.com>
2023-06-15 14:52:44 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+// Deel 1
 
-// Stap 1: Verbinding maken met de database
+
 $servername = "localhost:3307";
 $username = "test";
 $password = "root";
@@ -13,15 +14,16 @@ try {
     die("Verbinding met de database mislukt: " . $e->getMessage());
 }
 
-// Stap 2: De query uitvoeren om alle data te selecteren
-$sql = "SELECT * FROM jouw_tabelnaam";
-$result = $conn->query($sql);
 
-// Stap 3: Controleren of er resultaten zijn en deze weergeven
-if ($result->rowCount() > 0) {
+$sql = "SELECT * FROM producten";
+$stmt = $conn->query($sql);
+
+
+if ($stmt->rowCount() > 0) {
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo "<table>";
     echo "<tr><th>ID</th><th>Naam</th><th>Beschrijving</th></tr>";
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    foreach ($result as $row) {
         echo "<tr>";
         echo "<td>" . $row["id"] . "</td>";
         echo "<td>" . $row["naam"] . "</td>";
@@ -33,18 +35,79 @@ if ($result->rowCount() > 0) {
     echo "Geen resultaten gevonden.";
 }
 
-// Stap 4: Verbinding met de database sluiten
+
 $conn = null;
 ?>
-In this script, we establish a connection to the database using PDO, execute the query, fetch the results, and display them in a table format. Finally, we close the database connection. Make sure to replace "jouw_gebruikersnaam", "jouw_wachtwoord", "winkel", and "jouw_tabelnaam" with the appropriate values for your database and table.
+
+<?php
+// Deel 2
+
+$servername = "localhost:3307";
+$username = "test";
+$password = "root";
+$dbname = "winkel";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Verbinding met de database mislukt: " . $e->getMessage());
+}
 
 
+$productCode = 1;
+$sql = "SELECT * FROM producten WHERE product_code = :productCode";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':productCode', $productCode);
+$stmt->execute();
 
 
+if ($stmt->rowCount() > 0) {
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "Product Code: " . $row["product_code"] . "<br>";
+    echo "Product Naam: " . $row["product_naam"] . "<br>";
+    echo "Beschrijving: " . $row["beschrijving"] . "<br>";
+} else {
+    echo "Geen resultaten gevonden.";
+}
 
 
+$conn = null;
+?>
+
+<?php
+// Deel 3
 
 
+$servername = "localhost:3307";
+$username = "test";
+$password = "root";
+$dbname = "winkel";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Verbinding met de database mislukt: " . $e->getMessage());
+}
 
 
+$productCode = 2;
+$sql = "SELECT * FROM producten WHERE product_code = :productCode";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':productCode', $productCode);
+$stmt->execute();
 
+
+if ($stmt->rowCount() > 0) {
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "Product Code: " . $row["product_code"] . "<br>";
+    echo "Product Naam: " . $row["product_naam"] . "<br>";
+    echo "Beschrijving: " . $row["beschrijving"] . "<br>";
+} else {
+    echo "Geen resultaten gevonden.";
+}
+
+
+$conn = null;
+?>
--- select.php
+++ select.php
@@ -1,113 +1,56 @@
-<?php
-// Deel 1
+<?php 
 
 
-$servername = "localhost:3307";
-$username = "test";
-$password = "root";
-$dbname = "winkel";
+$host = 'localhost:3307';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbinding met de database mislukt: " . $e->getMessage());
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "connection succes";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
+// $sql = "SELECT * FROM producten" ;
+// $result = $pdo->query($sql);
 
-$sql = "SELECT * FROM producten";
-$stmt = $conn->query($sql);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$product_naam = $_POST["product_naam"];
+$prijs_per_stuk = $_POST["prijs_per_stuk"];
+$omschrijving = $_POST["omschrijving"];
 
+$sql = "INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES (?, ?, ?)";
+$stmt = $pdo->prepare($sql);
+$resultaat = $stmt->execute([$product_naam, $prijs_per_stuk, $omschrijving]);
 
-if ($stmt->rowCount() > 0) {
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Naam</th><th>Beschrijving</th></tr>";
-    foreach ($result as $row) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["naam"] . "</td>";
-        echo "<td>" . $row["beschrijving"] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "Geen resultaten gevonden.";
+echo "Data successfully inserted.";
 }
-
-
-$conn = null;
 ?>
 
-<?php
-// Deel 2
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    
+<form method="POST" action="insert.php">
+  <input type="text" name="product_naam" placeholder="product_naam" required> <br>
+  <input type="number" name="prijs_per_stuk" placeholder="prijs_per_stuk" required> <br>
+  <input type="text" name="omschrijving" placeholder="omschrijving" required> <br>
+  <input type="submit" value="Verstuur">
+</form>
+</body>
 
-$servername = "localhost:3307";
-$username = "test";
-$password = "root";
-$dbname = "winkel";
+</html>
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbinding met de database mislukt: " . $e->getMessage());
-}
-
-
-$productCode = 1;
-$sql = "SELECT * FROM producten WHERE product_code = :productCode";
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':productCode', $productCode);
-$stmt->execute();
-
-
-if ($stmt->rowCount() > 0) {
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo "Product Code: " . $row["product_code"] . "<br>";
-    echo "Product Naam: " . $row["product_naam"] . "<br>";
-    echo "Beschrijving: " . $row["beschrijving"] . "<br>";
-} else {
-    echo "Geen resultaten gevonden.";
-}
-
-
-$conn = null;
-?>
-
-<?php
-// Deel 3
-
-
-$servername = "localhost:3307";
-$username = "test";
-$password = "root";
-$dbname = "winkel";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbinding met de database mislukt: " . $e->getMessage());
-}
-
-
-$productCode = 2;
-$sql = "SELECT * FROM producten WHERE product_code = :productCode";
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':productCode', $productCode);
-$stmt->execute();
-
-
-if ($stmt->rowCount() > 0) {
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo "Product Code: " . $row["product_code"] . "<br>";
-    echo "Product Naam: " . $row["product_naam"] . "<br>";
-    echo "Beschrijving: " . $row["beschrijving"] . "<br>";
-} else {
-    echo "Geen resultaten gevonden.";
-}
-
-
-$conn = null;
-?>
